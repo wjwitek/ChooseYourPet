e59cb9e12e312d90b4ecfedcb41a60358d8d71b9
LVala <84684231+LVala@users.noreply.github.com>
2022-12-22 20:26:11 +0100

    Add csv handling functions and endpoint for criteria descriptions

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from utils.data import get_criteria_info, get_pet_data
+
+criteria = get_criteria_info("./data/categories.csv")
+pet_data = get_pet_data("./data/pet_data.csv", [i["name"] for i in criteria])
+# TODO: criteria matrices from pet_data
 
 app = Flask(__name__, static_folder="./assets/build", static_url_path="/")
 
 @app.route("/")
-def hello_world():
+def get_index():
     return app.send_static_file("index.html")
+
+@app.route("/resources/criteria")
+def get_criteria():
+    return {"criteria": criteria}
