ca0d3824b43de7b60825a33eee1b124905219897
wjwitek <weronikaj.witek@gmail.com>
2023-01-21 15:36:57 +0100

    add group decision making using aip

--- utils/ahp.py
+++ utils/ahp.py
@@ -1,5 +1,5 @@
 import numpy as np
-import random
+import sys
 
 RI = {3: 0.546, 4: 0.83, 5: 1.08, 6: 1.26, 7: 1.33, 8: 1.41, 9: 1.45}
 
@@ -24,6 +24,9 @@ def calculate_consistency(matrix: np.ndarray) -> float:
     ci = (eigen_values.astype('float64').max() - n) / (n - 1)
     return ci / RI[n]
 
+def geo_mean(array: np.ndarray):
+    return array.prod()**(1.0/len(array))
+
 class AnalyticHierarchyProcess:
     def __init__(self, pets_data: list[dict[str, str]]):
         self.pets_data = pets_data
@@ -34,10 +37,10 @@ class AnalyticHierarchyProcess:
         self.expert_number = None
 
     def is_criteria_set(self, expert_id: int) -> bool:
-        return self.criteria_matrix[expert_id] is not None
+        return expert_id in self.criteria_matrix.keys() and self.criteria_matrix[expert_id] is not None
 
     def is_pets_set(self, expert_id: int) -> bool:
-        return self.pets_matrices[expert_id] is not None
+        return expert_id in self.pets_matrices.keys() and self.pets_matrices[expert_id] is not None
 
     def is_ready(self) -> bool:
         if len(self.criteria_matrix.keys()) != self.expert_number:
@@ -69,17 +72,30 @@ class AnalyticHierarchyProcess:
 
         return {"criteria": criteria_consistency, "pets": pets_consistencies}
 
-    def choose_pet(self) -> list[dict]:
-        if not self.is_ready(): return None, None
-
-        criteria_vector = calculate_priority_vector(self.criteria_matrix[1])
+    def calculate_rank_vector(self, expert_id) -> np.ndarray:
+        criteria_vector = calculate_priority_vector(self.criteria_matrix[expert_id])
         criteria_vector = criteria_vector.astype("float64")
 
         rank_vector = np.zeros(len(self.pets_data))
         for i in range(len(self.pets_matrices)):
-            pet_criteria_vector = calculate_priority_vector(self.pets_matrices[1][i])
+            pet_criteria_vector = calculate_priority_vector(self.pets_matrices[expert_id][i])
             rank_vector += pet_criteria_vector.astype("float64") * criteria_vector[i]
 
+        return rank_vector
+
+    def choose_pet(self) -> list[dict]:
+        if not self.is_ready(): return None, None
+
+        rank_vectors = []
+
+        for key in self.criteria_matrix.keys():
+            rank_vectors.append(self.calculate_rank_vector(key))
+
+        # aggregate individual priorities using geometric mean
+        rank_vectors = np.asarray(rank_vectors)
+        rank_vector = [geo_mean(rank_vectors[:, i]) for i in range(rank_vectors.shape[1])]
+
+        # return 3 highest results
         indexes = [i for i in range(len(self.pets_data))]
         result = [x for _, x in sorted(zip(rank_vector, indexes), reverse=True)][:3]
         result = [self.pets_data[i] for i in result]
